6a8b3259a4cfe85111fb2523b752fd06f5788a89
Yixin Tian <yixin0829@outlook.com>
2022-09-23 15:45:12 -0400

    activity 4 bonus

--- hello.py
+++ hello.py
@@ -1,20 +1,38 @@
 from distutils.log import debug
+from email import message
+from xml.dom import ValidationErr
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+import re
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Regexp, ValidationError
+
+class RegValidator(object):
+    '''
+    We will create a custom validator class for Flask-WTF with custom error message
+    The validator checks if the string contains a substring 'key'
+    '''
+    def __init__(self, key:str='@') -> None:
+        self.regex=f'.*{key}.*'
+        self.key = key
+
+    def __call__(self, form, field):
+        if not re.match(self.regex, field.data):
+            raise ValidationError(f'Please include an \'{self.key}\' in the email address. \'{field.data}\' is missing an \'{self.key}\'.')
 
 class NameForm(FlaskForm):
- name = StringField('What is your name?', validators=[DataRequired()])
- submit = SubmitField('Submit')
-
+    utoronto_validator = RegValidator('utoronto')
+    email_validator = RegValidator('@')
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[email_validator])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '1234'
+app.config['SECRET_KEY'] = '123'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -30,15 +48,27 @@ def user(name):
 # activity 4
 @app.route('/form', methods=['GET', 'POST'])
 def form():
-    name = None
     form = NameForm()
+    ut_email_f = 0
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!') # needs to be rendered in html template
-            session['name'] = form.name.data # better pratice: use user session to store variables
-            return redirect(url_for('form')) # trick: post/redirect/get_pattern
-    return render_template('form.html', form=form, name=session.get('name'))
+
+        if old_email is not None and old_email !=form.email.data:
+            flash('Looks like you have changed your email!')
+
+        if re.match('.*utoronto.*', form.email.data):
+            ut_email_f = 1
+
+        session['name'] = form.name.data # better pratice: use user session to store variables
+        session['email'] = form.email.data
+        return redirect(url_for('form')) # trick: post/redirect/get_pattern
+    
+    # get updated name and email before rendering
+    print(f'{ut_email_f}')
+    return render_template('form.html', form=form, name=session.get('name'), email=session.get('email'), ut_email_f=ut_email_f)
 
 if __name__ == '__main__':
     app.run(debug=True)
