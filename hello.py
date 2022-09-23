986976a9c2fb6708682c3aaf8b07b9b84dfba1b6
Yixin Tian <yixin0829@outlook.com>
2022-09-23 14:03:33 -0400

    Activity 4 ex4-7

--- hello.py
+++ hello.py
@@ -1,10 +1,20 @@
 from distutils.log import debug
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+ name = StringField('What is your name?', validators=[DataRequired()])
+ submit = SubmitField('Submit')
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -16,6 +26,19 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+# activity 4
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!') # needs to be rendered in html template
+            session['name'] = form.name.data # better pratice: use user session to store variables
+            return redirect(url_for('form')) # trick: post/redirect/get_pattern
+    return render_template('form.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
     app.run(debug=True)
