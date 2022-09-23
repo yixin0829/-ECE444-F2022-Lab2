cbc9cf5f13b1c6e17b23db3cef0ab44fe665e35d
Yixin Tian <yixin0829@outlook.com>
2022-09-23 11:45:01 -0400

    activity 3 commit

--- hello.py
+++ hello.py
@@ -1,11 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
+from distutils.log import debug
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html', current_time=datetime.utcnow())
 
 # ex2.2: dynamic routing
 @app.route('/user/<name>')
 def user(name):
-    return f'<h1>Hello, {name}!</h1>'
+    return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
