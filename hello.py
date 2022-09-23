b5373173715237441ea54c37b011382a58a84442
Yixin Tian <yixin0829@DESKTOP-NR9OAKQ.localdomain>
2022-09-23 10:26:30 -0400

    ex2.2: dynamic routing

--- hello.py
+++ hello.py
@@ -3,4 +3,9 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
- return '<h1>Hello World!</h1>'
+    return '<h1>Hello World!</h1>'
+
+# ex2.2: dynamic routing
+@app.route('/user/<name>')
+def user(name):
+    return f'<h1>Hello, {name}!</h1>'
